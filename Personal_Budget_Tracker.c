8bd33b01db643252b07844e89200519669dd1865
Jordan <120746510+JordznFish@users.noreply.github.com>
2025-05-30 22:36:21 +0800

    Update Personal_Budget_Tracker.c

--- Personal_Budget_Tracker.c
+++ Personal_Budget_Tracker.c
@@ -1,11 +1,46 @@
 /*
-FLowchart:
-Log In(struct) -> Display Menu -> Get User Choice -> Save(Export to File) -> Exit
 
-##Add Expenses##
--Budget goal warning (Set Monthly Budget e.g. $10,000(NTD) per month)
--Display remaining budget in menu 
--LOCK budget if exceed
+流程總覽:
+1)用戶登入(支持創建多個用戶)
+
+2)進入使用者專屬介面
+-顯示 7 個主要選單功能
+- 若有設定每月消費限制，會在畫面上方顯示各月份預算
+
+3)使用者可依照選單操作，直到選擇退出系統
+
+選項:
+1.記帳（Add Expenses）
+- 手動輸入日期、類別、金額與 備註(optional)
+- 自動產生UNIQUE交易 ID（TH-XXXX）
+- 若本月總額將超過設定預算，會提醒用戶超支，但仍允許寫入
+
+2.刪除特定款項（Remove Expenses）
+-使用交易 ID 來刪除特定消費紀錄
+-由於每款項都是唯一的ID，所以不會有刪錯的問題
+
+3.顯示全部歷史紀錄（View History Transactions）
+- 印出所有消費紀錄，並加總顯示總支出金額
+
+4.顯示特定月份的消費記錄 （View Monthly Report）
+- 輸入月份（YYYY-MM），只顯示該月支出
+
+5.轉成CSV 
+- 將 <用戶>.txt(用戶數據) 檔案轉為 CSV 格式
+- 若有在設定選項設定預算，會同時將 <用戶>_budget.txt 也轉為 CSV
+
+6. 設定
+- 目前支援「設定每月預算」
+- 若已設定過某月，則會進行「覆寫」更新，可輕易調整和新增預算於其他月份
+- 所有預算儲存在 <用戶>_budget.txt
+
+7. 離開系統 (Quit)
+
+資料檔案說明: 
+- users.txt：紀錄所有用戶帳密
+- <用戶>.txt：紀錄該用戶的消費紀錄
+- <用戶>_budget.txt：紀錄每月預算限制
+- <用戶>.csv 和 <用戶>_budget.csv：轉出後可用 Excel 開啟的報表
 
 */
 #include <stdio.h>
@@ -20,6 +55,7 @@ Log In(struct) -> Display Menu -> Get User Choice -> Save(Export to File) -> Exi
 #define MAX_PASSWORD 50
 #define USER_FILE "users.txt"
 #define DATE_FORMAT 10
+#define MAX_LINE 256
 
 typedef enum
 {
@@ -40,11 +76,32 @@ typedef struct
 
 void display_menu(const User *p_current_user)
 {
-    char *menu_choices[OPTIONS] = {"Add Expenses", "Remove Expenses", "View history transactions", "View Monthly Report", "Export to CSV", "Settings", "Exit"};
+    char *menu_choices[OPTIONS] = {"Add Expenses", "Remove Expenses", "View History Transactions", "View Monthly Report", "Export to CSV", "Settings", "Exit"};
 
     printf("=======================\n");
     printf("Personal Budget Tracker\n");
     printf("Welcome back! %s\n", p_current_user->name);
+    
+    char budget_filename[100];
+    sprintf(budget_filename, "%s_budget.txt", p_current_user->name);
+    FILE *budget_limit_fp;
+    budget_limit_fp = fopen(budget_filename, "r");
+
+    if (budget_limit_fp)
+    {
+        printf("-----------------------\n");
+        printf("Budget limit:\n");
+        char line[MAX_LINE];
+        char budget_month[8];
+        float budget_amount;
+        while (fgets(line, sizeof(line), budget_limit_fp))
+        {
+            sscanf(line, "%[^|]|%f", budget_month, &budget_amount);
+            printf("%s: $%.2f\n", budget_month, budget_amount);
+        }
+        fclose(budget_limit_fp);
+    }
+
     printf("=======================\n");
     for (int i = 0; i < OPTIONS; i++)
     {
@@ -66,7 +123,7 @@ int get_user_choice(void)
 User login(void)
 {
     User current_user;
-    char line[100];
+    char line[MAX_LINE];
     bool isUserFound = false;
     bool isValidPass = false;
 
@@ -165,7 +222,7 @@ void add_expenses(User *p_current_user)
     bool isDuplicate;
     
     //Get Unqiue_ID by checking no duplicate id exist 
-    char line[100];
+    char line[MAX_LINE];
     FILE *read_fp;
     
     //Generate ID
@@ -224,6 +281,55 @@ void add_expenses(User *p_current_user)
         while (getchar() != '\n');
     }
 
+    //Budget warning check
+    char budget_filename[100];
+    sprintf(budget_filename, "%s_budget.txt", p_current_user->name);
+    FILE *budget_fp;
+    budget_fp = fopen(budget_filename, "r");
+
+    if (budget_fp)
+    {
+        char budget_line[MAX_LINE];
+        char budget_date[8];
+        float budget_limit;
+        while (fgets(budget_line, sizeof(budget_line), budget_fp))
+        {
+            sscanf(budget_line, "%[^|]|%f", budget_date, &budget_limit);
+            if (strncmp(budget_date, date, 7) == 0)
+            {
+                char total_spent_filename[100], line[MAX_LINE];
+                sprintf(total_spent_filename, "%s.txt", p_current_user->name);
+                FILE *read_total_spent_fp;
+                read_total_spent_fp = fopen(total_spent_filename, "r");
+
+                float total_month_expenses = 0;
+                if (read_total_spent_fp)
+                {
+                    char temp_ID[50], temp_date[50], temp_category[50], temp_description[100];
+                    float temp_amount;
+
+                    while (fgets(line, sizeof(line), read_total_spent_fp))
+                    {
+                        sscanf(line, "%[^|]|%[^|]|%[^|]|%f|%[^\n]", temp_ID, temp_date, temp_category, &temp_amount, temp_description);
+                        if (strncmp(temp_date, date, 7) == 0)
+                        {
+                            total_month_expenses += temp_amount;
+                        }
+                    }
+                    fclose(read_total_spent_fp);
+                }
+
+                if (total_month_expenses + amount > budget_limit)
+                {
+                    printf("Warning: This expenses will exceed the monthly budget $%.2f!\n", budget_limit);
+                    float exceeded = (total_month_expenses + amount) - budget_limit;
+                    printf("You are exceeding your budget by: $%.2f\n", exceeded);
+                }
+            }
+        }
+    fclose(budget_fp);
+    }
+    
     //Ask for descriptions
     char ask_w_description;
     char description[100];
@@ -282,7 +388,7 @@ void remove_expenses_by_itemID(User *p_current_user)
     return;
     }
 
-    char line[100], ID[50];
+    char line[MAX_LINE], ID[50];
     bool found_ID = false;
 
     while (fgets(line, sizeof(line), fp))
@@ -327,7 +433,7 @@ void view_history_reports(User *p_current_user)
     }
 
     //Display all transactions
-    char line[100];
+    char line[MAX_LINE];
     float total = 0;
 
     printf("====== Transaction History ======\n");
@@ -407,7 +513,7 @@ void view_monthly_report(User *p_current_user)
     return;
 }
 
-void export_to_CSV(User *p_current_user)
+void export_expenses_to_CSV(User *p_current_user)
 {
     //Load file
     char txt_filename[100], csv_filename[100];
@@ -425,12 +531,14 @@ void export_to_CSV(User *p_current_user)
         return;
     }
 
+    //Header
     fprintf(destination, "ID,Date,Category,Amount,Description\n");
+
     //Read txt file
-    char line[100];
+    char line[MAX_LINE];
     while (fgets(line, sizeof(line), source))
     {
-        //replace | to , easier for python
+        //replace | to , (easier for excel)
         for (int i = 0; line[i]; i++)
         {
             if (line[i] == '|')
@@ -450,7 +558,172 @@ void export_to_CSV(User *p_current_user)
     return;
 }
 
-void settings(){};
+void export_budget_to_CSV(User *p_current_user)
+{
+    //Load File 
+    char budget_txt_filename[100], budget_csv_filename[100];
+    sprintf(budget_txt_filename, "%s_budget.txt", p_current_user->name);
+    sprintf(budget_csv_filename, "%s_budget.csv", p_current_user->name);
+
+    FILE *source = fopen(budget_txt_filename, "r");
+    FILE *destination = fopen(budget_csv_filename, "w");
+
+    if (!destination || !source)
+    {
+        printf("Error occured\n");
+        return;
+    }
+
+    //Header
+    fprintf(destination, "Date,Budget\n");
+
+    //Read txt file 
+    char line[MAX_LINE];
+
+    while (fgets(line, sizeof(line), source))
+    {
+        //replace | to , (easier for excel)
+        for (int i = 0; line[i]; i++)
+        {
+            if (line[i] == '|')
+            {
+                line[i] = ',';
+            }
+        }
+        //Write csv file
+        fprintf(destination, "%s", line);
+    }
+
+    fclose(source);
+    fclose(destination);
+
+    printf("%s exported to %s successfully.\n", budget_txt_filename, budget_csv_filename);
+
+    return;
+}
+
+void set_monthly_budget(User *p_current_user)
+{
+    //Prepare file name (budget file & expenses file)
+    char budget_filename[100];
+    char filename[100];
+    sprintf(budget_filename, "%s_budget.txt", p_current_user->name);
+    sprintf(filename, "%s.txt", p_current_user->name);
+    
+    //Get valid month with edge case handling
+    char month[8];
+    printf("Enter Month to set budget (YYYY-MM): ");
+    fgets(month, sizeof(month), stdin);
+    month[strcspn(month, "\n")] = 0;
+
+    while (strlen(month) != 7)
+    {
+        printf("Invalid format! Please try again (YYYY-MM): ");
+        fgets(month, sizeof(month), stdin);
+        month[strcspn(month, "\n")] = 0;
+    }
+
+    //Calculate total expenses for the specific month (from John.txt)
+    FILE *fp_expense;
+    fp_expense = fopen(filename, "r");
+
+    char line[MAX_LINE];
+    char ID[50], date[50], category[50], description[100];
+    float amount, monthly_total = 0;
+    while (fgets(line, sizeof(line), fp_expense))
+    {
+        sscanf(line, "%[^|]|%[^|]|%[^|]|%f|%[^\n]", ID, date, category, &amount, description);
+        if (strncmp(date, month, 7) == 0)
+        {
+            monthly_total += amount;
+        }
+    }
+    fclose(fp_expense);
+
+    //if new budget_limit is lower than expense = invalid 
+    float budget_limit;
+    do
+    {
+        printf("Total spent in %s: $%.2f\n", month, monthly_total);
+        printf("Enter budget limit (must be larger or equal to $%.2f): ", monthly_total);
+        scanf("%f", &budget_limit);
+        while (getchar() != '\n');
+
+        if (budget_limit < monthly_total)
+        {
+            printf("Invalid budget limit! Try again!\n");
+        }
+    } while (budget_limit < monthly_total);
+    
+    //Update or set budget limit to particular month
+    FILE *fp_old = fopen(budget_filename, "r");
+    FILE *fp_temp = fopen("temp_budget.txt", "w");
+
+    if (!fp_temp)
+    {
+        printf("Error creating temp file.\n");
+        if (fp_old) fclose(fp_old);
+        return;
+    }
+
+    char budget_line[MAX_LINE], existing_month[8];
+    float existing_limit;
+    bool updated = false;
+
+    if (fp_old)
+    {
+        while (fgets(budget_line, sizeof(budget_line), fp_old))
+        {
+            sscanf(budget_line, "%[^|]|%f", existing_month, &existing_limit);
+            if (strcmp(existing_month, month) != 0) fputs(budget_line, fp_temp); // keep other months
+            else updated = true; // skip current month (will overwrite)
+        }
+        fclose(fp_old);
+    }
+
+    // Append or updated new monthly budget
+    fprintf(fp_temp, "%s|%.2f\n", month, budget_limit);
+    fclose(fp_temp);
+
+    remove(budget_filename);
+    rename("temp_budget.txt", budget_filename);
+
+    if (updated)
+        printf("Updated budget for %s to $%.2f\n", month, budget_limit);
+    else
+        printf("Set new budget for %s: $%.2f\n", month, budget_limit);
+}
+
+void settings(User *p_current_user)
+{
+    //Display settings
+    char *option_settings[2] = {"Set Monthly Budget", "Coming soon..."};
+    printf("----- Settings -----\n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%d. %s\n", i+1, option_settings[i]);
+    }
+
+    //Prompt user to enter choice
+    int userChoice_settings;
+    printf("Choice: ");
+    scanf("%d", &userChoice_settings);
+    while (getchar() != '\n');
+
+    switch (userChoice_settings)
+    {
+    case 1:
+        set_monthly_budget(p_current_user);
+        break;
+    case 2:
+        printf("Stay tuned for future updates!\n");
+        break;
+    default:
+        printf("Invalid Option!\n");
+        break;
+    }
+    return;
+}
 
 int main(void)
 {
@@ -495,10 +768,11 @@ int main(void)
             view_monthly_report(p_current_user);
             break;
         case EXPORT_TO_CSV:
-            export_to_CSV(p_current_user);
+            export_expenses_to_CSV(p_current_user);
+            export_budget_to_CSV(p_current_user);
             break;
         case SETTINGS:
-            ;
+            settings(p_current_user);
             break;
         case QUIT:
             printf("Thanks for using our program!\n");
